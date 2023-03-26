21e6b15b229d92ed1c2ebc2f10fbd6c1eedbeed9
cindoushka <cindie-baj@live.fr>
2023-03-27 01:43:13 +0200

    end of 1st exercice on python

--- lejustprix.py
+++ lejustprix.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 
-
 objets = {"ordinateur": 2000, "iphone": 1400, "godemichet": 200, "womanizer": 350}
 
 print(objets["ordinateur"])
@@ -9,13 +8,17 @@ vitrine = 0
 
 for objet in objets.values():
     vitrine += objet
-    print(vitrine)
 
 limite = 10
 tours = 0
 
 while tours < limite:
-    prix = int(input("Quel est le juste prix?"))
+    try: prix = int(input("Quel est le juste prix?"))
+
+    except ValueError:
+        print("Vous devez proposer un nombre entier positif")
+        tours == tours
+        continue 
     if prix > vitrine:
         print("C'est moins!")
     elif prix < vitrine :
@@ -24,9 +27,11 @@ while tours < limite:
         print("C'est gagné!")
         tours = 10
     tours += 1
+    print(tours)
     if tours < (limite-2):
-        print(f"Heureusement, il te reste {tours} essais")
+        print(f"Heureusement, il te reste {limite-tours} essais")
     elif tours < limite:
-        print(f"Attention, tu n'as plus que {tours} essais")
+        print(f"Attention, tu n'as plus que {limite-tours} essais")
     elif tours == limite: 
         print ("C'est perdu!")
+
